047ee44588fe6861d26c6376d2aab833daef3660
notGonim <mhmodgonim96@gmail.com>
2021-03-24 11:42:01 +0200

    DONE WITH THE MAIN CONFIG FOR THE TASK

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import { errors } from './middlewares/error.js'
 
 
 export const app = express()
@@ -10,3 +11,9 @@ app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 
+//to handle all the routes that related to 
+
+
+
+//middleware to handle all errors 
+app.use(errors)
