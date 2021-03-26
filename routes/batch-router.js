e642e55e043df79bfd83d10d83d3cfb5996598d3
Mhmod Gonim <69145711+notGonim@users.noreply.github.com>
2021-03-26 19:45:50 +0200

    Update batch-router.js

--- routes/batch-router.js
+++ routes/batch-router.js
@@ -7,6 +7,12 @@ export const BatchRouter = express.Router()
 
 
 BatchRouter.post('/batch/new', createBatch)
-BatchRouter.get('/batch', getBatchesCnt)
+BatchRouter.get('/batch', getBatches)
+
+/*
+-- use this route if you want to get certain batches just use color and size words to filter data
+-- api/batch?color=red&&size=S
+BatchRouter.get('/batch', getBatchesCnt )
+*/
 
 
